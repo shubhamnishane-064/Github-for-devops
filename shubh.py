4570dfcdd603e1ac073eb08f460e3ed4fc9901d5
shubhamnishane-064 <shubhamnishane064@gmail.com>
2026-02-04 08:57:54 +0530

    the

--- shubh.py
+++ shubh.py
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 #!/bash/bin
 echo "enter the name of the student"
+=======
+echo "the repository is given bellow"
+echo "the repository id is shubhamnishane064@gmail.com"
+>>>>>>> 98e9dab (the new changes is added)
